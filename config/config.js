f129d0ba7618cd77180d81ceacfcf83cd3b0ae58
Benjamin Ashbaugh <scitronboy@gmail.com>
2020-08-10 13:23:01 -0600

    handle no cred file

--- config/config.js
+++ config/config.js
@@ -1,9 +1,14 @@
 const _ = require('lodash');
 
 const config = require('./config.json');
-const credentials = require('./credentials.json');
+let credConfig;
+try {
+  const credentials = require('./credentials.json');
+  credConfig = credentials;
+} catch {
+  credConfig = {};
+}
 const defaultConfig = config.development;
-const credConfig = credentials;
 const environment = process.env.NODE_ENV || 'development';
 const envConfig = config[environment];
 const finalConfig = _.merge(defaultConfig, credConfig, envConfig);
